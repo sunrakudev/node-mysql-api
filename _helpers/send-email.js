0aa0ba015094f0069def70dfb2fdf579addb73a7
Earl <you@example.com>
2026-05-22 05:35:59 +0800

    Fix SMTP secure flag for port 465

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -3,9 +3,11 @@ const nodemailer = require('nodemailer');
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html }) {
+    const port = parseInt(process.env.SMTP_PORT);
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port,
+        secure: port === 465,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
