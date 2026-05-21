b90a1c3c442674a49898c2c4484df3340c2c4034
Earl <you@example.com>
2026-05-22 05:41:11 +0800

    Switch to Resend SDK for email sending

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -1,20 +1,10 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html }) {
-    const port = parseInt(process.env.SMTP_PORT);
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port,
-        secure: port === 465,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    });
-
-    await transporter.sendMail({
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    await resend.emails.send({
         from: process.env.EMAIL_FROM,
         to,
         subject,
