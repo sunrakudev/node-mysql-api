b8722ba6f9f57b59a21a0b51025ab4498ac67c8f
Earl <you@example.com>
2026-05-22 06:00:09 +0800

    Revert to Nodemailer SMTP with Mailtrap

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -1,10 +1,20 @@
-const { Resend } = require('resend');
+const nodemailer = require('nodemailer');
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html }) {
-    const resend = new Resend(process.env.RESEND_API_KEY);
-    await resend.emails.send({
+    const port = parseInt(process.env.SMTP_PORT);
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port,
+        secure: port === 465,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transporter.sendMail({
         from: process.env.EMAIL_FROM,
         to,
         subject,
