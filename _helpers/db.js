bf9021892db46dfc98ec1dcd25ab4d8151a6ea19
Earl <you@example.com>
2026-05-22 19:59:26 +0800

    Support DB_NAME env var alongside DB_DATABASE

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes, Op } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
+    process.env.DB_DATABASE || process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
